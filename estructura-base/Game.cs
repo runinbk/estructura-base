6177d74bf65b5a0ca149fe25c217ae9eff81d531
runinbk <runinbk@gmail.com>
2025-01-04 05:57:28 -0400

    feat(init): Create project with OpenTK and create Game class

---
+++ estructura-base/Game.cs
@@ -0,0 +1,38 @@
+﻿using OpenTK.Graphics.OpenGL4;
+using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+namespace estructura_base
+{
+    internal class Game : GameWindow
+
+    {
+        private float angle = 0.0f;
+        private float angleHorizontal = 0.0f;
+        private bool isMouseDown = false;
+        private Vector2 lastMousePos;
+        private float pitch = 0.0f;
+        private float yaw = 0.0f;
+        private float zoom = 2.0f;
+
+        public Game(int width, int height, string title) 
+            : base(GameWindowSettings.Default, new NativeWindowSettings() { Size = (width, height), Title = title })
+        {
+            VSync = VSyncMode.On;
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+
+            base.OnUpdateFrame(e);
+
+            if (KeyboardState.IsKeyDown(Keys.Escape))
+            {
+                Close();
+            }
+        }
+
+    }
+}
--- estructura-base/Program.cs
+++ estructura-base/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+namespace estructura_base
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Iniciando aplicación...");
+                using (Game game = new Game(800, 800, "LearnOpenTK"))
+                {
+                    game.Run();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+                Console.WriteLine($"StackTrace: {ex.StackTrace}");
+                Console.ReadLine(); // Para mantener la consola abierta
+            }
+        }
+    }
+}
