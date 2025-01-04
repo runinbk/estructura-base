1e5d2420d287c7194db4b175bfbc29c861bf7720
runinbk <runinbk@gmail.com>
2025-01-04 07:12:52 -0400

    feat(Game): Creating additional functions to execute the triangle

--- estructura-base/Game.cs
+++ estructura-base/Game.cs
@@ -1,4 +1,5 @@
-﻿using OpenTK.Graphics.OpenGL4;
+﻿using estructura_base.Shaders;
+using OpenTK.Graphics.OpenGL4;
 using OpenTK.Mathematics;
 using OpenTK.Windowing.Common;
 using OpenTK.Windowing.Desktop;
@@ -6,33 +7,95 @@ using OpenTK.Windowing.GraphicsLibraryFramework;
 
 namespace estructura_base
 {
-    internal class Game : GameWindow
-
+    public class Game : GameWindow
     {
-        private float angle = 0.0f;
-        private float angleHorizontal = 0.0f;
-        private bool isMouseDown = false;
-        private Vector2 lastMousePos;
-        private float pitch = 0.0f;
-        private float yaw = 0.0f;
-        private float zoom = 2.0f;
+        private readonly float[] _vertices =
+        {
+            -0.5f, -0.5f, 0.0f, // Bottom-left vertex
+             0.5f, -0.5f, 0.0f, // Bottom-right vertex
+             0.0f,  0.5f, 0.0f  // Top vertex
+        };
+
+        private int _vertexBufferObject;
+
+        private int _vertexArrayObject;
+
+        private Shader _shader;
 
         public Game(int width, int height, string title) 
-            : base(GameWindowSettings.Default, new NativeWindowSettings() { Size = (width, height), Title = title })
+            : base(GameWindowSettings.Default, new NativeWindowSettings() { Size = (width, height), Title = title }) 
         {
-            VSync = VSyncMode.On;
+        
+        }
+
+        protected override void OnLoad()
+        {
+            base.OnLoad();
+
+            GL.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+            _vertexBufferObject = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ArrayBuffer, _vertexBufferObject);
+
+            GL.BufferData(BufferTarget.ArrayBuffer, _vertices.Length * sizeof(float), _vertices, BufferUsageHint.StaticDraw);
+
+            _vertexArrayObject = GL.GenVertexArray();
+            GL.BindVertexArray(_vertexArrayObject);
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
+
+            GL.EnableVertexAttribArray(0);
+
+            _shader = new Shader("Shaders/shader.vert", "Shaders/shader.frag");
+
+            _shader.Use();
+
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            base.OnRenderFrame(e);
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            _shader.Use();
+
+            GL.BindVertexArray(_vertexArrayObject);
+
+            GL.DrawArrays(PrimitiveType.Triangles, 0, 3);
+            SwapBuffers();
         }
 
         protected override void OnUpdateFrame(FrameEventArgs e)
         {
-
             base.OnUpdateFrame(e);
 
-            if (KeyboardState.IsKeyDown(Keys.Escape))
+            var input = KeyboardState;
+
+            if (input.IsKeyDown(Keys.Escape))
             {
                 Close();
             }
         }
 
+        protected override void OnResize(ResizeEventArgs e)
+        {
+            base.OnResize(e);
+
+            GL.Viewport(0, 0, Size.X, Size.Y);
+        }
+
+        protected override void OnUnload()
+        {
+            // Unbind all the resources by binding the targets to 0/null.
+            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+            GL.BindVertexArray(0);
+            GL.UseProgram(0);
+
+            // Delete all the resources.
+            GL.DeleteBuffer(_vertexBufferObject);
+            GL.DeleteVertexArray(_vertexArrayObject);
+
+            GL.DeleteProgram(_shader.Handle);
+
+            base.OnUnload();
+        }
     }
 }
